f499ee5d1666944ef7768d5bc5895ebdb3fa4ba3
Mikhail <wavednight@gmail.com>
2023-12-04 23:53:45 +0700

    Задача4

--- Homework004/Program.cs
+++ Homework004/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿void showDigits(int num)
+{
+    int[] digits = new int[10];
+    int count = 0;
+
+    Console.WriteLine("Число: " + num);
+    while (num != 0)
+    {
+        digits[count++] = num % 10;
+        num /= 10;
+    }
+
+    while (count > 0)
+    {
+        Console.Write(digits[--count]);
+        if (count != 0)
+        {
+            Console.Write(",");
+        }
+    }
+    Console.WriteLine();
+}
+
+
+showDigits(9542);
+showDigits(8);
+showDigits(568);
