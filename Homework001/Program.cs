6e7588d283a946fc9f18972af0d268c31468ced3
Mikhail <wavednight@gmail.com>
2023-12-04 23:05:58 +0700

    Задача 1

--- Homework001/Program.cs
+++ Homework001/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿void main (int num)
+{
+    if (num % 7 == 0 && num % 23 == 0)
+    {
+        Console.WriteLine("Число кратно и 7, и 23.");
+    }
+    else
+    {
+        Console.WriteLine("Число не кратно одновременно 7 и 23.");
+    }
+
+    Console.ReadLine();
+}
+
+main(50);
+main(322);
+main(7);
+main(322);
