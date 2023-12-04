766e25425f10c52218beb48f42f197ee0b08d7d8
Mikhail <wavednight@gmail.com>
2023-12-04 23:47:59 +0700

    Задача3

--- Homework003/Program.cs
+++ Homework003/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int MaxDigit(int number)
+{
+    int max = 0;
+    while (number !=0)
+    {
+        int digit = number % 10;
+        number /= 10;
+        if (digit > max)
+        {
+            max = digit;
+        }
+    }
+    return max;
+}
+int num = 78;
+
+Console.WriteLine("Source = " + num);
+Console.WriteLine($"Max digit is {MaxDigit(num)}");
